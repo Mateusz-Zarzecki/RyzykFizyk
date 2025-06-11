f23492f7672acab8b95e0a7ed958e3a23593590e
Krystian-Tomczyk <krystian.tomczyk@tm1.edu.pl>
2025-06-12 00:57:25 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function errorAlert() {
-    let params = new URL(window.location.toString()).searchParams;
-    let message = params.has("info") ? params.get("info") : null;
-    console.log(message);
-    if(message) {
-        alert(message);
-    }
-}
